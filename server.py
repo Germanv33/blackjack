ee2409c715fbe64d2311f07ba876eaebd858060e
Germanv33 <71227097+Germanv33@users.noreply.github.com>
2021-12-18 17:52:00 +0300

    New adds, server

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import socket
 import threading
-
+from src.game import Game
+from src.player import Player
+import src.ui as UI
 
 def main():
     host = "localhost"
@@ -10,7 +12,7 @@ def main():
         server.bind((host, port))
         server.listen(2)
 
-        player_contact = []
+        player_contact = {}
         players_threads = []
 
         print("Waiting for new connections...")
@@ -34,8 +36,88 @@ def main():
                         thread.start()
                     connected = True
 
-            # TODO: Game loop
+            else:
+                game = Game(player_contact[0]["name"], player_contact[1]["name"])
+                
+                # TODO: Game loop
+                # 
+                # send first cards with ui
+                game.start_round()
 
+                frst_conn = player_contact[0]["conn"]
+                sec_conn = player_contact[1]["conn"]
+
+                frst_conn.send(str(game).encode())
+                sec_conn.send(str(game).encode())
+                #
+                # def choice
+                #                     1. get 1 more card          2. Ready
+                # (1) send 1 card to the player, then update information of second player about player 1's new card
+                # (2) Next player
+
+                player_1_turn = True
+
+                if player_1_turn:
+                    frst_conn.send(f"[Player 1's turn]".encode())
+                    sec_conn.send(f"[Player 1's turn]".encode())
+                    if frst_conn.recv(1024).decode() == "True":
+                        if game.give_card(0):
+                            continue
+                    player_1_turn = False
+                    player_2_turn = True
+                    
+                if player_2_turn:
+                    frst_conn.send(f"[Player 2's turn]".encode())
+                    sec_conn.send(f"[Player 2's turn]".encode())
+                    if frst_conn.recv(1024).decode() == "True":
+                        if game.give_card(1):
+                            continue
+                    player_2_turn = False
+                
+                if not (player_1_turn and player_2_turn):
+                    if game.dealer_needs_buy():
+                        connection.send("[Dealer's turn]".encode())
+                        
+                        game.give_card(2)
+                    else:
+                        connection.send("[End of round]".encode())
+                        
+                        if game.dealer_won():
+                            connection.send(
+                                "Sorry, you lost this time...".encode())
+                        else:
+                            connection.send("Nice! You won!".encode())
+                        break
+
+
+
+                # after dealer choice, send dealer card to players
+
+                # again def choice 
+                # if someone didnt loose after card pick, or just ready and have <= 21.
+                # send game result
+
+                # sart new game by sending new cards.
+
+            
+
+def send_first_cards(player_contact, game):
+
+    game.start_round()
+
+    frst_conn = player_contact[0]["conn"]
+    sec_conn = player_contact[1]["conn"]
+
+    frst_conn.send(str(game).encode())
+    sec_conn.send(str(game).encode())
+    
+    return game
+
+
+
+def game_choice(player_contact, game):
+    
+           
 
 def get_clients_info(connection, player_contact):
     connection.send("Введите свое имя:".encode())
@@ -43,5 +125,5 @@ def get_clients_info(connection, player_contact):
     player_contact.append({"conn": connection, "name": client_name})
 
 
-if name == "main":
+if __name__ == "__main__":
     main()
